cca6a4c89cd08c567d1b3a1e9c4b2e54597215fe
sindhu0582 <sindhu0582@gmail.com>
2025-07-18 18:19:42 +0530

    first commit

---
+++ hello_world.java
@@ -0,0 +1,13 @@
+package wipro;
+
+public class hello_world {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 30;
+		int mul = a * 3;
+		System.out.println("a multiplied by 3 equals to :"+mul);
+
+	}
+
+}
